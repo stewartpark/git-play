08b44e669583dd91a5ad74acc4be6595af31a8e9
Ju-yeong Park <jyp@ndtech.co>
2013-04-03 22:59:47 +0900

    Uploaded to PyPI.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
+try:
+    ldsc = open("README.md").read()
+except:
+    ldsc = ""
+
 setup(
     name="git-play",
-    version="0.1dev",
+    version="0.03",
     author="Ju-yeong Park",
     author_email="interruptz@gmail.com",
     scripts=["bin/git-play"],
     url="http://interruptz.github.com/git-play",
     license="LICENSE",
     description="Git-play is a command line tool for deploying an application server easily from remote git repository.",
-    long_description = open("README.md").read(),
+    long_description = ldsc,
     install_requires=[
         'PyYAML==3.10',
         'GitPython==0.3.2.RC1'      
