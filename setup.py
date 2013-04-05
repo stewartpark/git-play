b52804a8a853c827e18e60bac0658ae129f3d26a
Ju-yeong Park <jyp@ndtech.co>
2013-04-06 00:09:35 +0900

    Version up to v0.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except:
 
 setup(
     name="git-play",
-    version="0.09",
+    version="0.10",
     author="Ju-yeong Park",
     author_email="interruptz@gmail.com",
     scripts=["bin/git-play"],
